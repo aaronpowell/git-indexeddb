748ee38d3051249c4fcb2e3d308342967dab4a47
Aaron Powell <me@aaron-powell.com>
2013-10-23 16:01:34 +1100

    implementing get

--- indexeddb.js
+++ indexeddb.js
@@ -3,13 +3,16 @@
 
     var isHash = /^[a-z0-9]{40}$/;
     var version = 1;
-    var storeName = 'hashs';
-    var indexKey = 'hash';
+    var hashStoreName = 'hashs';
+    var hashIndexKey = 'hash';
+    var pathStoreName = 'paths';
+    var pathIndexKey = 'path';
 
     exports.gitIndexedDB = function (prefix) {
         var context = {};
         return {
-            init: init.bind(context, prefix)
+            init: init.bind(context, prefix),
+            get: get.bind(context)
         };
     };
 
@@ -20,9 +23,13 @@
         request.addEventListender('upgradeNeeded', function (e) {
             var db = e.target.result;
 
-            var hashStore = db.createObjectStore(storeName);
+            var hashStore = db.createObjectStore(hashStoreName);
+            hashStore.createIndex(hashIndexKey, hashIndexKey, {
+                unique: true
+            });
 
-            hashStore.createIndex(indexKey, indexKey, {
+            var pathStore = db.createObjectStore(pathStoreName);
+            pathStore.createIndex(pathIndexKey, pathIndexKey, {
                 unique: true
             });
         });
@@ -35,5 +42,38 @@
             throw e;
         });
     };
+
+    var get = function get(key, callback) {
+        var context = this;
+        if (!callback) {
+            return get.bind(this, key);
+        }
+        if (isHash.test(key)) {
+            var transaction = context.db.transaction(hashStoreName);
+            var store = transaction.objectStore(hashStoreName);
+
+            var request = store.get(key);
+
+            request.addEventListender('success', function (e) {
+                //pretty sure if it goes in as Uint8Array it comes out as such
+                callback(null, e.target.result);
+            });
+            request.addEventListender('error', function (e) {
+                throw e;
+            });
+        } else {
+            var transaction = context.db.transaction(pathStoreName);
+            var store = transaction.objectStore(pathStoreName);
+
+            var request = store.get(key);
+
+            request.addEventListender('success', function (e) {
+                callback(null, e.target.result);
+            });
+            request.addEventListender('error', function (e) {
+                throw e;
+            });
+        }
+    }
 
 })(window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB, window);
